8f987a564a12647e8f5b8ea76640aeccf5d3e933
Fauzy169 <ahmadfauzy169@gmail.com>
2024-01-21 18:18:03 +0800

    Tugas Project assigment 01 - Ahmad Fausi Febrian

--- main.js
+++ main.js
@@ -20,18 +20,17 @@ for (let key of keys) {
 
 			display_output.innerHTML = CleanOutput(result);
 		}    else if (value == "negate") {
-            // Mengubah tanda angka
             if (isNegated) {
-                // Menghapus tanda negatif jika sudah ada
+                
                 if (input.charAt(0) === '-') {
                     input = input.slice(1);
                 }
             } else {
-                // Menambah tanda negatif jika belum ada
+               
                 input = '-' + input;
             }
 
-            isNegated = !isNegated; // Mengubah status negasi
+            isNegated = !isNegated;
             display_input.innerHTML = CleanInput(input);
         } else {
 			if (ValidateInput(value)) {
@@ -136,35 +135,33 @@ function PerpareInput (input) {
 
 const darkBtn = document.querySelector('.dark');
 const lightBtn = document.querySelector('.light');
-darkBtn.style = "opacity: 0.1"; //Default Theme is Dark!
+darkBtn.style = "opacity: 0.1";
 
-//If Dark Theme button Clicked, then switch to dark theme
 darkBtn.addEventListener('click', () => {
 	document.documentElement.classList.remove("light");
 	document.documentElement.classList.add("dark");
-	window.localStorage.setItem('mode', 'dark'); //Set mode value to dark and save it, if user refresh the page, the browser can access this value
+	window.localStorage.setItem('mode', 'dark'); 
 	darkBtn.style = "opacity: 0.5";
 	lightBtn.style = "opacity: 1";
 });
 
-//if Light Theme button Clicked, then switch yo light theme
 lightBtn.addEventListener('click', () => {
 	document.documentElement.classList.remove("dark");
 	document.documentElement.classList.add("light");
-	window.localStorage.setItem('mode', 'light');//Set mode value to light and save it, if user refresh the page, the browser can access this value
+	window.localStorage.setItem('mode', 'light');
 	darkBtn.style = "opacity: 1";
 	lightBtn.style = "opacity: 0.5";
 });
 
-const mode = window.localStorage.getItem('mode');//grab mode value
-if (mode == 'dark') { //if user set the theme to dark, then load dark theme
+const mode = window.localStorage.getItem('mode');
+if (mode == 'dark') { 
 	document.documentElement.classList.remove("light");
 	document.documentElement.classList.add("dark");
 	darkBtn.style = "opacity: 0.5";
 	lightBtn.style = "opacity: 1";
 }
 
-if (mode == 'light') { //if user set the theme to light, then load light theme
+if (mode == 'light') { 
 	document.documentElement.classList.remove("dark");
 	document.documentElement.classList.add("light");
 	darkBtn.style = "opacity: 1";
